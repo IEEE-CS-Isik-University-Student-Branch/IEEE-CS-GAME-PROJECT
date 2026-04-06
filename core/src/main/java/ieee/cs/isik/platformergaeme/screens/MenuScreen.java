0b4e7fb39b69fdd12bd08fbb41f46d44123644d9
Frozetr <ardafiratyilmaz1@gmail.com>
2026-04-06 18:45:43 +0300

    Initializes the menu UI and input when this screen is shown.

--- core/src/main/java/ieee/cs/isik/platformergaeme/screens/MenuScreen.java
+++ core/src/main/java/ieee/cs/isik/platformergaeme/screens/MenuScreen.java
@@ -2,16 +2,43 @@ package ieee.cs.isik.platformergaeme.screens;
 
 import com.badlogic.gdx.ApplicationListener;
 import com.badlogic.gdx.Game;
+import com.badlogic.gdx.Gdx;
 import com.badlogic.gdx.Screen;
+import com.badlogic.gdx.graphics.Texture;
+import com.badlogic.gdx.graphics.g2d.BitmapFont;
+import com.badlogic.gdx.scenes.scene2d.Stage;
+import com.badlogic.gdx.scenes.scene2d.ui.Button;
+import com.badlogic.gdx.scenes.scene2d.ui.Skin;
+import com.badlogic.gdx.scenes.scene2d.ui.TextButton;
+import com.badlogic.gdx.scenes.scene2d.utils.ClickListener;
+import com.badlogic.gdx.scenes.scene2d.utils.TextureRegionDrawable;
 
 public class MenuScreen implements Screen {
-
+    private Stage stage;
+    private Texture buttontexture;
     /**
      * Called when this screen becomes the current screen for a {@link Game}.
      */
+    /**
+     * Initializes the menu UI and input when this screen is shown.
+     */
     @Override
     public void show() {
+        stage= new Stage();
+        Gdx.input.setInputProcessor(stage);
+        buttontexture= new Texture(Gdx.files.internal("Buttons.png"));
 
+        TextureRegionDrawable drawable= new TextureRegionDrawable(buttontexture);
+        TextButton.TextButtonStyle style = new TextButton.TextButtonStyle();
+
+        style.up = drawable;
+        style.down = drawable;
+        style.font = new BitmapFont();
+
+        TextButton button = new TextButton("Play", style);
+        button.setPosition(200, 300);
+        button.addListener(new ClickListener());
+        stage.addActor(button);
     }
 
     /**
