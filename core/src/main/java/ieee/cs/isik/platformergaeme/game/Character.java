3b18f584cc754930874645fe4a30069e3bb41ce4
lahit1 <naifcanbasci@gmail.com>
2026-05-15 21:18:57 +0300

    Added entity pool and render characters. Also added main character for test

--- core/src/main/java/ieee/cs/isik/platformergaeme/game/Character.java
+++ core/src/main/java/ieee/cs/isik/platformergaeme/game/Character.java
@@ -1,9 +1,14 @@
 package ieee.cs.isik.platformergaeme.game;
 
 import com.badlogic.gdx.assets.AssetManager;
+import com.badlogic.gdx.math.Vector2;
 import com.badlogic.gdx.physics.box2d.Body;
 import org.jetbrains.annotations.NotNull;
 
 public interface Character {
+    /**
+     * Box size in pixels
+     */
+    public static final Vector2 boxSize = new Vector2(100, 100);
     public CharacterEntity loadEntity(final AssetManager assets, final int id, @NotNull Body body);
 }
